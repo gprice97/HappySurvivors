87329ed1edca9a120002cff5dfa1343f4f05ba8a
Gunnar Price <pricegunnar@gmail.com>
2024-01-06 11:47:35 -0500

    Player Hurtbox and Healthbar Added

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D #Adds code to all the code of CharacterBody2D, we extending that class to here
 
+signal health_depleted
+
+var health = 100.0
+
 #Need to define Physics Process function
 #Godot calls this whenever the game updates physics
 func _physics_process(delta):
@@ -12,3 +16,12 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation() #   $HappyBoo is equivalent to get_node("HappyBoo"), the $ is just a shortcut
 	else:								#	%HappyBoo works if you right click the node and hit Access as Unique name, kinda seems like static variables?
 		%HappyBoo.play_idle_animation()
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_enemies = %HurtBox.get_overlapping_bodies() #returns array of overlapping slimes
+	
+	if overlapping_enemies.size() > 0:
+		health -= DAMAGE_RATE * overlapping_enemies.size() * delta
+		%ProgressBar.value = health #Set the value of my progress bar to the new health value after damage
+		if health <= 0:
+			health_depleted.emit() #Sends a signal to tell the UI we died and need the Game Over Screen
